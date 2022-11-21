cf2de52d42645f9f56f440873da153c429cb2ea9
nasim-coder <khannasimahmad@gmail.com>
2022-11-21 20:24:57 +0530

    completed greeting and quote

--- todos.js
+++ todos.js
@@ -1,13 +1,46 @@
-// greeting, quote, and date time // top bar
+// greeting
+(function showGreeting() {
+    function getGreetings(currentHours) {
+        if (currentHours <= 12) {
+            return "Good Morning";
+        }
+        else if (currentHours <= 18 && currentHours > 12) {
+            return "Good After Noon";
+        } else {
+            return "Good Evening";
+        }
+    }
 
+    let today = new Date();
+    let currentHours = today.getHours();
+    let greeting = getGreetings(currentHours);
+    let name = localStorage.getItem("name");
+    
+    if (!name) {
+        name = prompt("Plase enter your name:");
+        localStorage.setItem('name', name);
+    }
 
+    document.querySelector(".greeting").innerHTML = greeting + " " + name;
+    document.querySelector(".time").innerHTML = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    document.querySelector('.date').innerHTML = new Date().toDateString();
+})();
+
+    (async function showQuote() {
+        let rnum = Math.floor(Math.random() * 1642);
+        let response = await fetch("https://nasim-coder.github.io/staticjsondata/quote.json");
+        let data = await response.json();
+        // console.log();
+        // console.log(data2[rnum].author);
+        document.querySelector('.quote').innerText = data[rnum].text;
+        document.querySelector('.author').innerText = "-" + data[rnum].author;
+    })();
 
 
 // to do creation logic
 let createTodoButton = document.querySelector(".btn");
 let todoInput = document.querySelector(".input");
 let todoContainer = document.querySelector('.todos-container');
-
 createTodoButton.addEventListener('click', function () {
     if (todoInput.value) {
         let todoElement = document.createElement('p');

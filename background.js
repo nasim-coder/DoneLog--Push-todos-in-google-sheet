0628bf7f1f5a394827d4ac45c48ec66122e44ba7
nasim-coder <khannasimahmad@gmail.com>
2023-12-25 14:41:01 +0530

    added background.js to open todos html on click, on command on context menu

---
+++ background.js
@@ -0,0 +1,56 @@
+chrome.action.onClicked.addListener(function() {
+    chrome.tabs.create({ url: chrome.runtime.getURL('todos.html') });
+  });
+
+// Function to create the context menu item
+function createContextMenuItem() {
+    chrome.contextMenus.create({
+      id: 'openLifeList',
+      title: 'LifeList',
+      contexts: ['page'] 
+    });
+  }
+  
+  // Event listener for context menu item click
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === 'openLifeList') {
+      chrome.tabs.create({ url: chrome.runtime.getURL('todos.html') });
+    }
+  });
+  
+  // Call the function to create the context menu item
+  createContextMenuItem();
+  
+
+
+  
+// Function to check if the shortcut conflicts with existing browser action shortcuts
+async function doesShortcutConflict(shortcut) {
+    const commands = await chrome.commands.getAll();
+    const conflicts = commands.some(command => command.shortcut === shortcut);
+    return conflicts;
+  }
+  
+  // Function to set the shortcut if no conflicts are found
+  async function setShortcut() {
+    const shortcut = 'Ctrl+Shift+L'; // Define your desired shortcut here
+    
+    const conflict = await doesShortcutConflict(shortcut);
+    if (!conflict) {
+      chrome.commands.update({ name: 'openLifeList', shortcut });
+    } else {
+      console.error('Shortcut conflicts with existing browser action shortcuts');
+      // Handle the conflict or choose a different shortcut
+    }
+  }
+  
+  // Set the shortcut on extension load or as required
+  setShortcut();
+  
+
+chrome.commands.onCommand.addListener(function(command) {
+    if (command === 'openLifeList') {
+      chrome.tabs.create({ url: chrome.runtime.getURL('todos.html') });
+    }
+  });
+  

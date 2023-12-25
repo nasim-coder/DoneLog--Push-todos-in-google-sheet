9433f482ab1d90282f10d851fa34fcd38fd225c1
nasim-coder <khannasimahmad@gmail.com>
2023-12-25 14:48:35 +0530

    removed popupjs file

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Listen for button click
-    document.getElementById('openTodos').addEventListener('click', function() {
-      chrome.tabs.create({ url: chrome.runtime.getURL('todos.html') });
-    });
-  });
